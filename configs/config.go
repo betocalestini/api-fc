e4527a5588413eb39971c4b9aac49de4f3e8a922
Roberto Calestini <betocalestini@gmail.com>
2023-01-05 22:29:03 -0300

    feat: add GetOs env alternative

--- configs/config.go
+++ configs/config.go
@@ -1,6 +1,9 @@
 package configs
 
 import (
+	"os"
+	"strconv"
+
 	"github.com/go-chi/jwtauth"
 	"github.com/spf13/viper"
 )
@@ -18,6 +21,7 @@ type conf struct {
 	TokenAuth     *jwtauth.JWTAuth
 }
 
+// LoadConfig loads the configuration from the .env file using viper
 func LoadConfig(path string) (*conf, error) {
 	var cfg *conf
 	viper.SetConfigName("app_config")
@@ -35,4 +39,21 @@ func LoadConfig(path string) (*conf, error) {
 	}
 	cfg.TokenAuth = jwtauth.New("HS256", []byte(cfg.JWTSecret), nil)
 	return cfg, nil
+}
+
+// LoadOsGetEnv loads the configuration from the environment variables using os.Getenv
+func LoadOsGetEnv() (*conf, error) {
+	var cfg *conf
+	cfg.DBDriver = os.Getenv("DB_DRIVER")
+	cfg.DBHost = os.Getenv("DB_HOST")
+	cfg.DBPort = os.Getenv("DB_PORT")
+	cfg.DBUser = os.Getenv("DB_USER")
+	cfg.DBPass = os.Getenv("DB_PASS")
+	cfg.DBName = os.Getenv("DB_NAME")
+	cfg.WebServerPort = os.Getenv("WEB_SERVER_PORT")
+	cfg.JWTSecret = os.Getenv("JWT_SECRET")
+	t, err := strconv.Atoi(os.Getenv("JWT_EXPIRES_IN"))
+	cfg.JWTExpiresIn = t
+	cfg.TokenAuth = jwtauth.New("HS256", []byte(cfg.JWTSecret), nil)
+	return cfg, err
 }
