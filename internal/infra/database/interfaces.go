20d53bdf27c51688b891fc5c99a3d66c09d22105
Roberto Calestini <betocalestini@gmail.com>
2023-01-05 22:30:42 -0300

    feat: add interfaces

---
+++ internal/infra/database/interfaces.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"github.com/betocalestini/api-fc/internal/entity"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	DB *gorm.DB
+}
+
+func NewUser(db *gorm.DB) *User {
+	return &User{DB: db}
+}
+
+func (u *User) Create(user *entity.User) error {
+	return u.DB.Create(user).Error
+}
+
+func (u *User) FindByEmail(email string) (*entity.User, error) {
+	var user entity.User
+	err := u.DB.Where("email = ?", email).First(&user).Error
+	return &user, err
+}
