6876c412d3103c1b43a0f5f63999107b519b8d5b
Roberto Calestini <betocalestini@gmail.com>
2023-01-09 21:11:48 -0300

    fix: change the correct name of file

--- internal/infra/database/user_db.go
+++ internal/infra/database/user_db.go
@@ -1,8 +1,24 @@
 package database
 
-import "github.com/betocalestini/api-fc/internal/entity"
+import (
+	"github.com/betocalestini/api-fc/internal/entity"
+	"gorm.io/gorm"
+)
 
-type UserInterface interface {
-	Create(user *entity.User) error
-	FindByEmail(email string) (*entity.User, error)
+type User struct {
+	DB *gorm.DB
+}
+
+func NewUser(db *gorm.DB) *User {
+	return &User{DB: db}
+}
+
+func (u *User) Create(user *entity.User) error {
+	return u.DB.Create(user).Error
+}
+
+func (u *User) FindByEmail(email string) (*entity.User, error) {
+	var user entity.User
+	err := u.DB.Where("email = ?", email).First(&user).Error
+	return &user, err
 }
