5d0a7378b737549fd5b7e276cfedc82f9d91a8e5
Roberto Calestini <betocalestini@gmail.com>
2023-01-09 21:10:24 -0300

    fix: change the correct file name

--- internal/infra/database/interfaces.go
+++ internal/infra/database/interfaces.go
@@ -1,24 +1,16 @@
 package database
 
-import (
-	"github.com/betocalestini/api-fc/internal/entity"
-	"gorm.io/gorm"
-)
+import "github.com/betocalestini/api-fc/internal/entity"
 
-type User struct {
-	DB *gorm.DB
+type UserInterface interface {
+	Create(user *entity.User) error
+	FindByEmail(email string) (*entity.User, error)
 }
 
-func NewUser(db *gorm.DB) *User {
-	return &User{DB: db}
-}
-
-func (u *User) Create(user *entity.User) error {
-	return u.DB.Create(user).Error
-}
-
-func (u *User) FindByEmail(email string) (*entity.User, error) {
-	var user entity.User
-	err := u.DB.Where("email = ?", email).First(&user).Error
-	return &user, err
+type ProductInterface interface {
+	Create(product *entity.Product) error
+	FindAll(page, limit int, sort string) ([]entity.Product, error)
+	FindByID(id string) (*entity.Product, error)
+	Update(product *entity.Product) error
+	Delete(id string) error
 }
