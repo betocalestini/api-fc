53ff93236d18bcdab69b1e8fbb7462a0d7d62e44
Roberto Calestini <betocalestini@gmail.com>
2023-01-09 21:12:01 -0300

    feat: adding user test file

--- internal/infra/database/user_db_test.go
+++ internal/infra/database/user_db_test.go
@@ -31,3 +31,24 @@ func Test_CreateUser(t *testing.T) {
 	assert.NotNil(t, userFound.Password)
 
 }
+
+func Test_FindByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.User{})
+
+	user, _ := entity.NewUser("Joao", "joao@teste.com", "123456")
+	userDB := NewUser(db)
+
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	userFound, err := userDB.FindByEmail(user.Email)
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotNil(t, userFound.Password)
+}
