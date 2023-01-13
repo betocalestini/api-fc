7053e702be33bfe013fbd2cc7f46c056659b2edb
Roberto Calestini <betocalestini@gmail.com>
2023-01-12 23:15:50 -0300

    feat: chi and handlers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/betocalestini/api-fc/configs"
+	"github.com/betocalestini/api-fc/internal/entity"
+	"github.com/betocalestini/api-fc/internal/infra/database"
+	"github.com/betocalestini/api-fc/internal/infra/webserver/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -11,5 +19,22 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(config.DBDriver)
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Post("/products", productHandler.CreateProduct)
+	r.Get("/products/{id}", productHandler.GetProduct)
+
+	fmt.Println("Servidor rodando na porta:", config.WebServerPort)
+	http.ListenAndServe(fmt.Sprintf(":%s", config.WebServerPort), r)
 }
---
+++ internal/infra/webserver/handlers/product_handlers.go
@@ -0,0 +1,58 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/betocalestini/api-fc/internal/dto"
+	"github.com/betocalestini/api-fc/internal/entity"
+	"github.com/betocalestini/api-fc/internal/infra/database"
+	"github.com/go-chi/chi"
+)
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *ProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	product, err := h.ProductDB.FindByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(product)
+
+}
