939bb6ac37b7804443fb54da71ad3e1ee3b307b1
Roberto Calestini <betocalestini@gmail.com>
2023-01-22 10:39:15 -0300

    feat: creates user dto

---
+++ internal/dto/dto.go
@@ -0,0 +1,12 @@
+package dto
+
+type CreateProductInput struct {
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+type CreateUserInput struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
